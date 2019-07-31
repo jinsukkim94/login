b84be3306dce7bcad52d09e34d77cba957557626
Jinsuk Kim <jinsukkim94@gmail.com>
2019-07-31 00:41:23 -0700

    try

--- server.js
+++ server.js
@@ -39,6 +39,6 @@ require('./config/Passport')(passport);
 // Routes
 app.use('/api/users', users);
 
-const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+const port = process.env.PORT || 8080; // process.env.port is Heroku's port if you choose to deploy the app there
 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
