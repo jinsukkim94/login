aa49734e620547ff11f16c2488167a6cef63e12e
Jinsuk Kim <jinsukkim94@gmail.com>
2019-07-31 02:44:39 -0700

    try

--- client/src/components/auth/LoggedIn.js
+++ client/src/components/auth/LoggedIn.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
 
 export default class LoggedIn extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			username: ''
+		}
+	}
+	fetchUser() {
+		this.fetchUser().then((res) => {
+			if(res.message) {
+				this.setState({
+					username: res
+				})
+			}
+		})
+	}
 	logOut() {
 		this.props.history.push('/');
 	}
 
 	render() {
+		this.fetchUser(); 
 		return (
 			<div className="container">
 				<div style={{ marginTop: '4rem' }} className="row">
 					<div className="col s8 offset-s2">
-						<div> Hello, !</div>
+						<div> Hello, {this.state.username}!</div>
 						<button
 							style={{
 								width: '150px',
