e44e27048370f8351e6c997d6e7ea23b429374bd
Jinsuk Kim <jinsukkim94@gmail.com>
2019-07-31 14:15:16 -0700

    fixedError

--- validations/Register.js
+++ validations/Register.js
@@ -1,11 +1,9 @@
 const Validator = require('validator');
 const isEmpty = require('is-empty');
-module.exports = function validateRegisterInput(body) {
+module.exports = function validateRegisterInput(data) {
 	let errors = {};
 
 	// console.log(body[0]);
-	var data = body;
-	console.log(data);
 	// Convert empty fields to an empty string so we can use validator functions
 	data.email = !isEmpty(data.email) ? data.email : '';
 	data.password = !isEmpty(data.password) ? data.password : '';
