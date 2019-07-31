6099bf143c80655fc065f2c087c6606e8efa76d3
Jinsuk Kim <jinsukkim94@gmail.com>
2019-07-30 18:54:53 -0700

    finished front-end, back-end

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+class App extends Component {
+	render() {
+		return (
+			<Router>
+				<div className="App">
+					<Route exact path="/" component={Landing} />
+					<Route exact path="/register" component={Register} />
+					<Route exact path="/login" component={Login} />
+				</div>
+			</Router>
+		);
+	}
+}
+export default App;
